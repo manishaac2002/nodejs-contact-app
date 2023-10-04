04952e83be74542fe95ff4f23d5d61580f89ab28
manishaac2002 <manishaac2002@gmail.com>
2023-10-04 13:00:42 +0100

    Bug: it is in the html the response format to be change in json  ,error in format.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,16 +3,16 @@ const getContacts = (request, response) => {
 }
 
 const createContacts = (request, response) => {
-    console.log("This is request body :",request.body);
-    const {name,email,phone}=request.body
-    if(!name || !email || !phone){
+    console.log("This is request body :", request.body);
+    const { name, email, phone } = request.body
+    if (!name || !email || !phone) {
         response.status(400)
-        throw new Error ("All fields are mandatory")
+        throw new Error("All fields are mandatory")
     }
     response.status(200).json({ message: " Create Contacts" })
 }
 
-const getContactWithId= (request, response) => {
+const getContactWithId = (request, response) => {
     response.status(200).json({ message: `Get Contacts for ${request.params.id}` })
 }
 
@@ -24,9 +24,10 @@ const deleteContact = (request, response) => {
     response.status(200).json({ message: `Delete contacts for ${request.params.id}` })
 }
 
-module.exports=   {
-        getContacts,
-        createContacts,
-        getContactWithId,
-        updateContact,
-        deleteContact} 
+module.exports = {
+    getContacts,
+    createContacts,
+    getContactWithId,
+    updateContact,
+    deleteContact
+} 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const { errorHandler } = require('./middleware/errorHandler.js')
 const dotenv = require('dotenv').config()
 
 const application = express()
 application.use(express.json())
+application.use(errorHandler)
+
 const port = process.env.PORT || 2000
 
 application.use("/api/contacts", require('./routers/contact.routers.js'))
