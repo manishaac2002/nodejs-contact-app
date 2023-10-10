735dff0d311b1612068e809435f70bc112975d58
manishaac2002 <manishaac2002@gmail.com>
2023-10-10 18:13:20 +0530

    bug

--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,6 +1,6 @@
 const errorHandler = (error, request, response, next) => {
     const statusCode = response.statusCode ? response.statusCode : 500
+    console.log("hey this is error handler");
     response.json({ message: error.message, stackTrace: error.stack })
 }
-console.log("hey this is error handler");
 module.exports = { errorHandler }
