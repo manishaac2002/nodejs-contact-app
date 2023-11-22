615eaadb0e046359a2edff0af051fb4ce8ea5fcc
manishaac2002 <manishaac2002@gmail.com>
2023-11-22 12:04:26 +0530

    feat: middleware.

--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,5 +1,24 @@
-const errorHandler = (error,request,response,next)=>{
-    const statusCode =response.statusCode ?response.statusCode : 500
-    response.json({message :error.message,stackTrace :error.stack})
+const errorHandler = (error, request, response, next) => {
+    const statusCode = response.statusCode ? response.statusCode : 500
+    switch (key) {
+        case 400:
+            response.json({
+                title: "Not Found",
+                message: error.message,
+                stackTrace: error.stack
+            })
+            break;
+        case 404:
+            response.json({
+                title: "Validation Failed",
+                message: error.message,
+                stackTrace: error.stack
+            })
+            break;
+
+        default:
+            break;
+    }
+
 }
-module.exports =errorHandler
+module.exports = errorHandler
