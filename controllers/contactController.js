afa22c379293dd1c51fe84c7caa825180e786ab9
manishaac2002 <manishaac2002@gmail.com>
2023-11-22 13:41:20 +0530

    feat: Error handling in middleware.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,7 +2,7 @@ const getContacts = (request, response) => {
     response.status(200).json({ message: "Get all contacts" })
 }
 
-const createContacts = (request, response) => {
+const createContacts = async(request, response) => {
     console.log("This is request body :", request.body);
     const { name, email, phone } = request.body
     if (!name || !email || !phone) {
@@ -12,15 +12,15 @@ const createContacts = (request, response) => {
     response.status(201).json({ message: " Create Contacts" })
 }
 
-const getContactWithId = (request, response) => {
+const getContactWithId = async(request, response) => {
     response.status(200).json({ message: `Get Contacts for ${request.params.id}` })
 }
 
-const updateContact = (request, response) => {
+const updateContact =async (request, response) => {
     response.status(200).json({ message: `Update contacts for ${request.params.id}` })
 }
 
-const deleteContact = (request, response) => {
+const deleteContact = async(request, response) => {
     response.status(200).json({ message: `Delete contacts for ${request.params.id}` })
 }
 const testingController = () => {
--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,22 +1,44 @@
+const {constants} =require('../constants.js') 
 const errorHandler = (error, request, response, next) => {
     const statusCode = response.statusCode ? response.statusCode : 500
     switch (key) {
-        case 400:
-            response.json({
-                title: "Not Found",
-                message: error.message,
-                stackTrace: error.stack
-            })
-            break;
-        case 404:
+        case constants.VALIDATION_ERROR:
             response.json({
                 title: "Validation Failed",
                 message: error.message,
                 stackTrace: error.stack
             })
             break;
+        case constants.UNAUTHORIZED:
+            response.json({
+                title: "Unauthorized",
+                message: error.message,
+                stackTrace: error.stack
+            })
+            break;
+        case constants.FORBIDDEN:
+            response.json({
+                title: "Forbidden",
+                message: error.message,
+                stackTrace: error.stack
+            })
+            break;
+        case constants.NOT_FOUND:
+            response.json({
+                title: "Not Found",
+                message: error.message,
+                stackTrace: error.stack
+            })
+        case constants.SERVER_ERROR:
+            response.json({
+                title: "Server Error",
+                message: error.message,
+                stackTrace: error.stack
+            })
+            break;
 
         default:
+            console.log("No Error..!");
             break;
     }
 
